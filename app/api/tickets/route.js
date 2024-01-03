a97c17d75a939bd6952fe2c38556d3a3ead9e0e8
Popescu Andrei <popescu.andreisk@yahoo.com>
2024-01-03 13:29:45 +0100

    API route handlers

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,11 +1,31 @@
 import { NextResponse } from "next/server";
 
-const GET = async () => {
+export const dynamic = "force-dynamic";
+
+export const GET = async () => {
   const res = await fetch("http://localhost:4000/tickets");
 
   const tickets = await res.json();
 
   return NextResponse.json(tickets, {
     status: 200,
+  });
+};
+
+export const POST = async (request) => {
+  const ticket = await request.json();
+
+  const res = await fetch("http://localhost:4000/tickets", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(ticket),
+  });
+
+  const newTicket = res.json();
+
+  return NextResponse.json(newTicket, {
+    status: 201,
   });
 };
